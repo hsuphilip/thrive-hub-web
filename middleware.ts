9423ef677fe775f303a0b9e95832e7faa666d96a
Philip Hsu <phhsu@MacBook-Air-2.local>
2026-03-24 21:01:46 -0700

    Exclude icon files from auth middleware

--- middleware.ts
+++ middleware.ts
@@ -38,5 +38,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|logo-icon.png).*)"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|logo-icon.png|apple-touch-icon.png|thrive-icon.png).*)"],
 };
